a7f70e6d5cfb4735468b37f0d60948fe292762cd
BenBurkholder <70002558+BenBurkholder@users.noreply.github.com>
2024-01-24 21:34:14 -0500

    Changed settings in Driver

--- src/XWing/driver.java
+++ src/XWing/driver.java
@@ -16,10 +16,13 @@ public class driver
 {
    public static void main(String[] args) {
         XWingGUI gui = new XWingGUI();
+        gui.setUndecorated(true);
         gui.setVisible(true);
         gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         gui.setTitle("Benjamin Burkholder's Board");
         gui.pack();
+        // gui.setSize((int)( 162 * 11.81), (int)(91 * 11.81));
+        gui.setSize(1920,1080);
         
         ShipDatabase game = new ShipDatabase();
         
